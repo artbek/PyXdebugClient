51c8509ead7521d19725eeec0909ea315e2d2efd
artbek <artist@artbek.co.uk>
2012-06-10 13:04:24 +0100

    Scroll to relevant line in the source code.

--- pydbg.py
+++ pydbg.py
@@ -19,9 +19,10 @@ def _get_attributes(s):
 
 class Handler:
 	_codeview = None
+	_open_file = None
 
 	def __init__(self):
-		self._codeview = builder.get_object("textview_code").get_buffer()
+		self._codeview = builder.get_object("textview_code")
 
 	def delete_window(self, *args):
 		debugger.stop()
@@ -29,21 +30,29 @@ class Handler:
 
 	def run(self, button):
 		sent, response = debugger.run()
-		self._codeview.set_text(response)
+		self._codeview.get_buffer().set_text(response)
 
 	def step_over(self, button):
 		sent, response = debugger.step_over()
 		(lineno, filename) = _get_attributes(response)
-		f = open(filename[1].replace('file://', ''), 'r')
 		print lineno
-		print filename[1].replace('file://', '')
-		print f
-		self._codeview.set_text(f.read())
-		f.close()
+		file_to_open = filename[1].replace('file://', '')
+		print file_to_open
+		if (self._open_file != file_to_open):
+			f = open(file_to_open, 'r')
+			self._open_file = file_to_open
+			print f
+			self._codeview.get_buffer().set_text(f.read())
+			f.close()
+
+		iter = self._codeview.get_buffer().get_start_iter()
+		iter.set_line(int(lineno[1]) - 1)
+		mark = self._codeview.get_buffer().create_mark(None, iter, True)
+		self._codeview.scroll_to_mark(mark, 0.0, True, 0.0, 0.5)
 
 	def step_into(self, button):
 		sent, response = debugger.step_into()
-		self._codeview.set_text(response)
+		self._codeview.get_buffer().set_text(response)
 
 
 builder = Gtk.Builder()
