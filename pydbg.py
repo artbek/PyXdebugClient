f5f16254d88af82eaf159453441b87f88f9f8fc0
artbek <artist@artbek.co.uk>
2012-07-02 21:02:52 +0100

    Don't load Engine here.

--- pydbg.py
+++ pydbg.py
@@ -1,14 +1,11 @@
-from engine import Engine
 from gui_handler import Handler
 from gi.repository import Gtk
 
 builder = Gtk.Builder()
 builder.add_from_file("gui.glade")
 
-
 builder.connect_signals(Handler(builder))
 window = builder.get_object("window_main")
 window.show_all()
-
 
 Gtk.main()
